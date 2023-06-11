89af7a1b57b22990f0ae75e4c67aae8d920aa3c8
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2023-06-11 18:56:33 +0200

    Update index.js

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Project({ title, description, img, url }) {
   return (
     <div className="project" onClick={() => window.open(url, "_blank")}>
-      <img className="projectImg" src={img} />
+      <img className="projectImg" src={img} alt="" />
       <div className="infos">
         <h2>{title}</h2>
         <h4>{description}</h4>
